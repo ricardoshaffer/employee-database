8c623d608b42e565455d041233dd4382d8c41a1e
Ricardo Shaffer <ricardo.ramiro.shaffer@gmail.com>
2020-06-11 21:15:29 -0400

    Updates

---
+++ precache-manifest.30429417530b72946d0b690518795900.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a5296e1e8edc1a6c208aae2641227744",
+    "url": "/employee-database/index.html"
+  },
+  {
+    "revision": "294da2f32d0b01fc6033",
+    "url": "/employee-database/static/css/2.af3c1da9.chunk.css"
+  },
+  {
+    "revision": "6a71fb3c9e3dadc3b690",
+    "url": "/employee-database/static/css/main.745d6a81.chunk.css"
+  },
+  {
+    "revision": "294da2f32d0b01fc6033",
+    "url": "/employee-database/static/js/2.c080b766.chunk.js"
+  },
+  {
+    "revision": "3453b8997016469371284a28c0e873e2",
+    "url": "/employee-database/static/js/2.c080b766.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6a71fb3c9e3dadc3b690",
+    "url": "/employee-database/static/js/main.a4d4548c.chunk.js"
+  },
+  {
+    "revision": "5104dbecbcb16d568cd7",
+    "url": "/employee-database/static/js/runtime-main.62e73df4.js"
+  }
+]);
