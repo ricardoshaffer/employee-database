f2e05f0d1697188e187e30799e4023df98bbf095
Ricardo Shaffer <ricardo.ramiro.shaffer@gmail.com>
2020-06-11 20:40:52 -0400

    added sticky function and hid mobile branding

--- public-db/src/components/table-header.js
+++ public-db/src/components/table-header.js
@@ -5,31 +5,27 @@ import Col from 'react-bootstrap/Col';
 import Container from 'react-bootstrap/Container';
 function TableHeader(props) {
     return (
+        <>
       <Container fluid>
-          <thead>
-              <Row>
+          <thead className="">
+              <Row className="employee-row sticky-top">
                   <Col></Col>
-                  <Col onClick={props.sortByName}>Name</Col>
-                  <Col>Phone</Col>
+                  <Col className="hover-pointer heading" onClick={props.sortByName}>Name</Col>
+                  <Col className="heading">Phone</Col>
               </Row>
           </thead>
-
-          <tbody>
+          <tbody className="employee-row">
               {props.results.map(result => (
-                  <Row key={result.login.uuid}>
-                   
-
+                  <Row key={result.login.uuid} className="employee-row">
                       <Col> <img
                       src={result.picture.medium} title={result.name.first + " " + result.name.last} alt={result.name.first + " " + result.name.last} /></Col>
-                      
                       <Col>{result.name.first + " " + result.name.last}  </Col>
-
                       <Col>{result.cell}</Col>
-                      <hr></hr>
                   </Row>
               ))}
           </tbody>
       </Container >
+      </>
   )
 }
 
