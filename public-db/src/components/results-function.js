5d15c761a822cc9cd76e866686859d75bdf19810
Ricardo Shaffer <ricardo.ramiro.shaffer@gmail.com>
2020-06-11 20:43:06 -0400

    modified search feature to include set to lowercase

--- public-db/src/components/results-function.js
+++ public-db/src/components/results-function.js
@@ -8,26 +8,25 @@ class Results extends Component {
     state = {
         search: "",
         employees: [],
-        filteredEmployees: [],
+        employeeSort: [],
         order: ""
 
     };
-
     componentDidMount() {
         API.getUsers().then(res => this.setState({
             employees: res.data.results,
-            filteredEmployees: res.data.results
+            employeeSort: res.data.results
         })).catch(err => console.log(err))
     }
 
     sortByName = () => {
-        const filtereds = this.state.filteredEmployees;
+        const filtereds = this.state.employeeSort;
         if (this.state.order === "asc") {
             const sorteds = filtereds.sort((a, b) => (a.name.first > b.name.first) ? 1 : -1)
             console.log(sorteds)
 
             this.setState({
-                filteredEmployees: sorteds,
+                employeeSort: sorteds,
                 order: "desc"
             })
         } else {
@@ -36,79 +35,51 @@ class Results extends Component {
             console.log(sorteds)
 
             this.setState({
-                filteredEmployees: sorteds,
+                employeeSort: sorteds,
                 order: "asc"
             })
 
         }
     }
-    //when input is changing it will dynamically show the associates names that match in the screen
-    handleInputChange = event => {
-
-        const employees = this.state.employees;
-        const UserInput = event.target.value;
-        const filteredEmployees = employees.filter(employee => employee.name.first.toLowerCase().indexOf(UserInput.toLowerCase()) > -1
-        )
-        this.setState({
-            //change the state of  filteredEmployes now it holds all the employes that matches users
-            // search and will be passed down in this state
-
-            filteredEmployees,
-
-        });
-
-
-    };
-
-
-    //API call triggered when page it's refreshed and  when application it's loaded 
-    employeeSearch = () => {
-        API.getUsers()
-            .then(res => this.setState({
-
-                //change their both states to hold all the data from the API call(all employess) and will be passed down trough props like that
-                //employee will remain the same and filteredEmployes will be changed and passed down during application's life. Employee will always hold all employess.
-                filteredEmployees: res.data.results,
-                employees: res.data.results
-            }))
-            .catch(err => console.log(err))
-    }
-
-    //when button search it's clicked
-    handleSearch = event => {
-        event.preventDefault();
-        if (!this.state.search) {
-            alert("Enter a name")
-        }
-        const { employees, search } = this.state;
-
-        //filters the object looking for the value that matches the value entered in the input box by the user  (search.this.state)
-        const filteredEmployees = employees.filter(employee => employee.name.first.toLowerCase().includes(search.toLowerCase()));
-
-        this.setState({
-            filteredEmployees
-        });
-    }
-
-
-
     render() {
-
         return (
             <div>
-
                 <SearchForm
                     employee={this.state.employees}
                     handleSearch={this.handleSearch}
                     handleInputChange={this.handleInputChange} />
-                <TableHeader results={this.state.filteredEmployees}
+                <TableHeader results={this.state.employeeSort}
                     sortByName={this.sortByName}
-
                 />
             </div >
-
-
         )
+    }
+    handleInputChange = event => {
+
+        const employees = this.state.employees;
+        const UserInput = event.target.value;
+        const employeeSort = employees.filter(employee => employee.name.first.toLowerCase().indexOf(UserInput.toLowerCase()) > -1
+        )
+        this.setState({
+            employeeSort,
+        });
+    };
+    employeeSearch = () => {
+        API.getUsers()
+            .then(res => this.setState({
+                employeeSort: res.data.results,
+                employees: res.data.results
+            }))
+            .catch(err => console.log(err))
+    }
+    handleSearch = event => {
+        event.preventDefault();
+        const { employees, search } = this.state;
+        const employeeSort = employees.filter(employee => employee.name.first.toLowerCase().includes(search.toLowerCase()));
+
+        this.setState({
+            employeeSort
+        });
     }
 }
 
